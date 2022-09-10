781d9a9b0c00d90ea7abab04a1696f53962b4d29
nazeeeh <nazihahmed55@gmail.com>
2022-09-10 17:28:48 +0100

    Update on the index.js file

--- src/component/Note.jsx
+++ src/component/Note.jsx
@@ -5,7 +5,7 @@ function Note(){
             <h1>Here are some Useful tips to get started</h1>
             <p>Firstly you have to note that not every carpe is a hero</p>
         </div>
-    )
+    ) 
 }
 
 export default Note;
